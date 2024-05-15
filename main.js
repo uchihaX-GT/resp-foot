e7638d9350681ebb0ceb761faae63ab27819c83e
uchihaX-GT <157300058+uchihaX-GT@users.noreply.github.com>
2024-05-15 23:08:11 +0530

    responsive

---
+++ main.js
@@ -0,0 +1,10 @@
+const navbarToggleBtn=document.querySelector('.navbar-toggle-btn');
+const navbarNav =document.querySelector('navbar-nav');
+
+
+const navbarToggleFunc= function (){
+    navbarToggleBtn.classList.toggle('active');
+    navbarNav.classList.toggle('.active');
+}
+
+navbarToggleBtn.addEventListener('click',navbarToggleFunc)
